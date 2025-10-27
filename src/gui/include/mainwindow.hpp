63954a561b260c4bf19bdeafc6b9caedce57de4a
rinsteryga <rinsterr@yandex.ru>
2025-10-27 23:36:39 +0300

    Added a custom title bar

--- src/gui/include/mainwindow.hpp
+++ src/gui/include/mainwindow.hpp
@@ -19,27 +19,48 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
 
 private:
-    void onButtonClicked();
+    void onMainButtonClicked();
+    void onCloseButtonClicked();
+    void onMinimizeButtonClicked();
 
     void setupUI();
+
     void setupWindowProperties();
+    void setupFaviconLabel();
+    void setupWindowTitleLabel();
+
     void setupLabelFont(QLabel *label_);
-    void setupMainLabel();
     void setupMainButtonFont();
+    
+    void setupMainLabel();
+
     void setupMainButton();
+
+    void setupCloseButton();
+    void setupMinimizeButton();
+
+    void setupTitleBar();
+
     void setupLayout();
 
     void openAuthWindow();
+
     void setupConnections();
 
     std::unique_ptr<QWidget> centralWidget_;
 
     std::unique_ptr<QLabel> mainLabel_;
+    std::unique_ptr<QLabel> faviconLabel_;
+    std::unique_ptr<QLabel> windowTitleLabel_;
 
     std::unique_ptr<QFont> labelFont_;
     std::unique_ptr<QFont> mainButtonFont_;
 
+    std::unique_ptr<QWidget> titleBar_;
+
     std::unique_ptr<QPushButton> mainButton_;
+    std::unique_ptr<QPushButton> closeButton_;
+    std::unique_ptr<QPushButton> minimizeButton_;
 
     std::unique_ptr<QVBoxLayout> mainLayout_;
 
--- src/gui/src/mainwindow.cpp
+++ src/gui/src/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "../include/mainwindow.hpp"
 
+#include <QHBoxLayout>
 #include <QMessageBox>
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), authWindow_(nullptr)
 {
+    setAttribute(Qt::WA_TranslucentBackground, false);
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     setupUI();
 }
 
@@ -16,6 +19,9 @@ void MainWindow::setupUI()
 
     setupMainLabel();
     setupMainButton();
+
+    setupTitleBar();
+    
     setupLayout();
     setupWindowProperties();
     setupConnections();
@@ -25,7 +31,7 @@ void MainWindow::setupLabelFont(QLabel *label_)
 {
     labelFont_ = std::make_unique<QFont>();
     labelFont_->setFamily("Cascadia");
-    labelFont_->setPixelSize(28);
+    labelFont_->setPixelSize(48);
     labelFont_->setBold(true);
     labelFont_->setItalic(true);
     label_->setFont(*labelFont_);
@@ -35,9 +41,7 @@ void MainWindow::setupLabelFont(QLabel *label_)
 
 void MainWindow::setupWindowProperties()
 {
-    setFixedSize(900, 700);
-    setWindowIcon(QIcon("../icon.ico"));
-    setWindowTitle("diploma project");
+    setWindowState(Qt::WindowMaximized);
     setStyleSheet("background-color: #1d201a28");
     setCursor(Qt::ArrowCursor);
 }
@@ -70,16 +74,122 @@ void MainWindow::setupMainButton()
                                "padding: 10px;");
 }
 
+void MainWindow::setupCloseButton()
+{
+    closeButton_ = std::make_unique<QPushButton>("x", titleBar_.get());
+    closeButton_->setFixedSize(30, 30);
+    closeButton_->setStyleSheet(
+        "QPushButton {"
+        "   background-color: transparent;"
+        "   color: white;"
+        "   border: none;"
+        "   font-size: 20px;"
+        "}"
+        "QPushButton:hover {"
+        "   background-color: #aa3333;"
+        "}"
+        "QPushButton:pressed {"
+        "   background-color: #aa3333;"
+        "}"
+        "QPushButton:focus {"
+        "   background-color: transparent;"
+        "   outline: none;"
+        "}"
+        "QPushButton:default {"
+        "   background-color: transparent;"
+        "}");
+    closeButton_->setCursor(Qt::PointingHandCursor);
+}
+
+void MainWindow::setupMinimizeButton()
+{
+    minimizeButton_ = std::make_unique<QPushButton>("_", titleBar_.get());
+    minimizeButton_->setFixedSize(30, 30);
+    minimizeButton_->setStyleSheet(
+        "QPushButton {"
+        "   background-color: transparent;"
+        "   color: white;"
+        "   border: none;"
+        "   font-size: 20px;"
+        "}"
+        "QPushButton:hover {"
+        "   background-color: #aa3333;"
+        "}"
+        "QPushButton:pressed {"
+        "   background-color: #aa3333;"
+        "}"
+        "QPushButton:focus {"
+        "   background-color: transparent;"
+        "   outline: none;"
+        "}"
+        "QPushButton:default {"
+        "   background-color: transparent;"
+        "}");
+    minimizeButton_->setCursor(Qt::PointingHandCursor);
+}
+
+void MainWindow::setupFaviconLabel()
+{
+    faviconLabel_ = std::make_unique<QLabel>(titleBar_.get());
+    qDebug() << "Setting favicon";
+    faviconLabel_->setPixmap(QIcon("icons/icon.ico").pixmap(24, 24));
+    faviconLabel_->setStyleSheet("background-color: transparent;");
+}
+
+void MainWindow::setupWindowTitleLabel()
+{
+    windowTitleLabel_ = std::make_unique<QLabel>("cppforge", titleBar_.get());
+    windowTitleLabel_->setStyleSheet("color: white; font-size: 14px; background-color: transparent;");
+}
+
+void MainWindow::setupTitleBar()
+{
+    titleBar_ = std::make_unique<QWidget>(this);
+    titleBar_->setFixedHeight(40);
+    titleBar_->setStyleSheet("background-color: #2d2d30;");
+
+    auto titleBarLayout = std::make_unique<QHBoxLayout>(titleBar_.get());
+    titleBarLayout->setContentsMargins(10, 0, 5, 0);
+    titleBarLayout->setSpacing(10);
+
+    setupFaviconLabel();
+    setupWindowTitleLabel();
+    
+    setupMinimizeButton();
+    setupCloseButton();
+
+    titleBarLayout->addWidget(faviconLabel_.get());
+    titleBarLayout->addWidget(windowTitleLabel_.get());
+
+    titleBarLayout->addStretch();
+    
+    titleBarLayout->addWidget(minimizeButton_.get());
+    titleBarLayout->addWidget(closeButton_.get());
+
+    titleBar_->setLayout(titleBarLayout.release());
+}
+
 void MainWindow::setupConnections()
 {
-    connect(mainButton_.get(), &QPushButton::clicked, this, &MainWindow::onButtonClicked);
+    connect(mainButton_.get(), &QPushButton::clicked, this, &MainWindow::onMainButtonClicked);
+    connect(closeButton_.get(), &QPushButton::clicked, this, &MainWindow::onCloseButtonClicked);
+    connect(minimizeButton_.get(), &QPushButton::clicked, this, &MainWindow::onMinimizeButtonClicked);
 }
 
 void MainWindow::setupLayout()
 {
     mainLayout_ = std::make_unique<QVBoxLayout>(centralWidget_.get());
+
+    mainLayout_->addWidget(titleBar_.get());
+
     mainLayout_->addWidget(mainLabel_.get());
     mainLayout_->addWidget(mainButton_.get());
+    
+    mainLayout_->setAlignment(mainLabel_.get(), Qt::AlignCenter);
+    mainLayout_->setAlignment(mainButton_.get(), Qt::AlignCenter);
+
+    
+    mainLayout_->setContentsMargins(0, 0, 0, 50);
 }
 
 void MainWindow::openAuthWindow()
@@ -96,8 +206,20 @@ void MainWindow::openAuthWindow()
     authWindow_->activateWindow();
 }
 
-void MainWindow::onButtonClicked()
+void MainWindow::onMainButtonClicked()
 {
-    qDebug() << "Button is clicked";
+    qDebug() << "Main button is clicked";
     openAuthWindow();
+}
+
+void MainWindow::onCloseButtonClicked()
+{
+    qDebug() << "Close button is clicked";
+    close();
+}
+
+void MainWindow::onMinimizeButtonClicked()
+{
+    qDebug() << "Minimize button is clicked";
+    showMinimized();
 }
