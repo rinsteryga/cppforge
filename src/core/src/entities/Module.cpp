9682982020977dd693102ce0f55e6cf79948366b
rinsteryga <rinsterr@yandex.ru>
2025-10-30 23:35:45 +0300

    Source code of the module entity

---
+++ src/core/src/entities/Module.cpp
@@ -0,0 +1,22 @@
+#include "../../include/entities/Module.hpp"
+
+namespace cppforge::entities
+{
+    Module::Module(int64_t id, const QString &name, const QString &description)
+        : id_(id), name_(name), description_(description) {}
+
+    uint64_t Module::getId() const
+    {
+        return id_;
+    }
+
+    QString Module::getName() const
+    {
+        return name_;
+    }
+
+    QString Module::getDescription() const
+    {
+        return description_;
+    }
+}
