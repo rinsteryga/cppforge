bd1025d87c1e685a9453724b901e883f4e4a4f59
rinsteryga <rinsterr@yandex.ru>
2025-10-30 23:33:57 +0300

    Added the entity for achievements

---
+++ src/core/include/entities/Achievement.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cstdint>
+#include <chrono>
+#include <QString>
+
+namespace cppforge
+{
+    namespace entities
+    {
+        class Achievement
+        {
+        public:
+            Achievement(int64_t id, const QString &name, const QString &description, const QString &iconPath,
+                        const std::chrono::system_clock::time_point &dateEarned);
+
+            uint64_t getId() const;
+            QString getName() const;
+            QString getDescription() const;
+            QString getIconPath() const;
+            std::chrono::system_clock::time_point getDateEarned() const;
+
+            bool operator<(const Achievement &other) const;
+            bool operator==(const Achievement &other) const;
+
+        private:
+            int64_t id_;
+            QString name_;
+            QString description_;
+
+            QString iconPath_;
+            std::chrono::system_clock::time_point dateEarned_;
+        };
+    } // namespace entities
+} // namespace cppforge
