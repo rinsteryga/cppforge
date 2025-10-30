8fde076516b38088c832ca3516316c3dd92cc1ff
rinsteryga <rinsterr@yandex.ru>
2025-10-30 23:34:36 +0300

    Added the entity for modules

---
+++ src/core/include/entities/Module.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <QString>
+
+namespace cppforge
+{
+    namespace entities
+    {
+        class Module
+        {
+        public:
+            Module(int64_t id, const QString &name, const QString &description);
+
+            uint64_t getId() const;
+            QString getName() const;
+            QString getDescription() const;
+
+        private:
+            int64_t id_;
+            QString name_;
+            QString description_;
+        };
+    } // namespace entities
+} // namespace cppforge
