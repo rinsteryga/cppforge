2d8a0f1d907f0c7c97c4cd3fb8300afc4805128c
rinsteryga <rinsterr@yandex.ru>
2025-10-30 23:35:18 +0300

    Source code of the achievement entity

---
+++ src/core/src/entities/Achievement.cpp
@@ -0,0 +1,43 @@
+#include "../../include/entities/Achievement.hpp"
+
+namespace cppforge::entities
+{
+    Achievement::Achievement(int64_t id, const QString &name, const QString &description, const QString &iconPath,
+                             const std::chrono::system_clock::time_point &dateEarned)
+        : id_(id), name_(name), description_(description), iconPath_(iconPath), dateEarned_(dateEarned) {}
+
+    uint64_t Achievement::getId() const
+    {
+        return id_;
+    }
+
+    QString Achievement::getName() const
+    {
+        return name_;
+    }
+
+    QString Achievement::getDescription() const
+    {
+        return description_;
+    }
+
+    QString Achievement::getIconPath() const
+    {
+        return iconPath_;
+    }
+
+    std::chrono::system_clock::time_point Achievement::getDateEarned() const
+    {
+        return dateEarned_;
+    }
+
+    bool Achievement::operator<(const Achievement &other) const
+    {
+        return id_ < other.id_;
+    }
+
+    bool Achievement::operator==(const Achievement &other) const
+    {
+        return id_ == other.id_;
+    }
+}
