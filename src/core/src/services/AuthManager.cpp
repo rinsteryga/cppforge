c4d36e7319ebde1baa13d11f8a4354f5db68e90e
rinsteryga <rinsterr@yandex.ru>
2025-11-13 23:42:58 +0300

    added the intemediate version of an auth service

--- src/core/src/services/AuthManager.cpp
+++ src/core/src/services/AuthManager.cpp
@@ -1,40 +1,33 @@
-#include "AuthManager.hpp"
+#include "../../include/services/AuthManager.hpp"
+#include "../../include/utils/security/PasswordHashGenerator.hpp"
 
-#include <QDebug>
-
-namespace cppforge
+namespace cppforge::services
 {
-    namespace services
+    AuthManager::AuthManager(QObject *parent)
+        : QObject(parent) {}
+
+    bool AuthManager::login(const QString &username, const QString &password)
     {
-
-        AuthManager::AuthManager(QObject *parent)
-            : QObject(parent)
-        {
-            // не готово
-        }
-
-        bool AuthManager::login(const QString &username_, const QString &password_)
-        {
-            if (users_.contains(username_) && users_[username_] == password_)
-            {
-                currentUser_ = username_;
-                qDebug() << "User signed in:" << currentUser_;
-                return true;
-            }
-            qDebug() << "Sign in failed for user:" << username_;
+        if (!users_.contains(username))
             return false;
-        }
 
-        bool AuthManager::registerUser(const QString &username_, const QString &email_, const QString &password_)
-        {
-            if (users_.contains(username_))
-            {
-                qDebug() << "Sign up failed. User already exists:" << username_;
-                return false;
-            }
-            users_.insert(username_, password_);
-            qDebug() << "User signed up successfully:" << username_;
-            return true;
-        }
-    } // namespace services
-} // namespace cppforge
+        const QString storedHash = users_.value(username);
+        if (!cppforge::utils::security::PasswordHashGenerator::verify(password, storedHash))
+            return false;
+
+        currentUser_ = username;
+        return true;
+    }
+
+    bool AuthManager::registerUser(const QString &username, const QString &email, const QString &password)
+    {
+        Q_UNUSED(email);
+
+        if (users_.contains(username))
+            return false;
+
+        const QString hash =  cppforge::utils::security::PasswordHashGenerator::generate(password);
+        users_.insert(username, hash);
+        return true;
+    }
+}
