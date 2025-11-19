6157aefe1715731c7b2071a6e6fbc1e1fbd96bec
rinsteryga <rinsterr@yandex.ru>
2025-11-19 23:54:55 +0300

    repositories: added the postgres user repository

---
+++ src/data/repositories/PgUserRepository.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../../core/include/repositories/IUserRepository.hpp"
+
+#include <cstdint>
+#include <optional>
+#include <QString>
+#include <QtSql/QSqlDatabase>
+
+namespace cppforge
+{
+    namespace repositories
+    {
+        class PgUserRepository : public IUserRepository
+        {
+        public:
+            explicit PgUserRepository(QSqlDatabase &database);
+
+            std::optional<entities::User> findByEmail(const QString& email) const override;
+            void save(entities::User user) override;
+
+        private:
+            QSqlDatabase& database_;
+        };
+    } // namespace repositories
+} // namespace cppforge
