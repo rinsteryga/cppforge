dfa4cf9125b5492d00ec6bd2f3946d40b6478be2
rinsteryga <rinsterr@yandex.ru>
2026-03-04 02:07:45 +0300

    lesson: optimize const reference allocations

--- src/core/src/entities/Lesson.cpp
+++ src/core/src/entities/Lesson.cpp
@@ -3,7 +3,9 @@
 namespace cppforge::entities
 {
     Lesson::Lesson(uint64_t id, uint64_t moduleId, const QString &title, const QString &content)
-        : id_(id), moduleId_(moduleId), title_(title), content_(content) {}
+        : id_(id), moduleId_(moduleId), title_(title), content_(content)
+    {
+    }
 
     uint64_t Lesson::getId() const
     {
@@ -15,13 +17,13 @@ namespace cppforge::entities
         return moduleId_;
     }
 
-    QString Lesson::getTitle() const
+    const QString &Lesson::getTitle() const
     {
         return title_;
     }
 
-    QString Lesson::getContent() const
+    const QString &Lesson::getContent() const
     {
         return content_;
     }
-}
+} // namespace cppforge::entities
