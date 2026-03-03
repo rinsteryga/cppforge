86486156e1b3e0cbe07ab9dcec01c1a8563f256a
rinsteryga <rinsterr@yandex.ru>
2026-03-04 02:07:43 +0300

    iuserrepository: optimize const reference allocations

--- src/core/include/repositories/IUserRepository.hpp
+++ src/core/include/repositories/IUserRepository.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <QString>
+
 #include <cstdint>
 #include <optional>
-#include <QString>
 
 namespace cppforge
 {
@@ -19,9 +20,9 @@ namespace cppforge
         public:
             virtual ~IUserRepository() = default;
 
-            virtual std::optional<entities::User> findByEmail(const QString& email) const = 0;
-            virtual std::optional<entities::User> findByUsername(const QString& username) const = 0;
-            virtual bool save(entities::User user) = 0;
+            virtual std::optional<entities::User> findByEmail(const QString &email) const = 0;
+            virtual std::optional<entities::User> findByUsername(const QString &username) const = 0;
+            virtual bool save(const entities::User &user) = 0;
         };
     } // namespace repositories
 } // namespace cppforge
