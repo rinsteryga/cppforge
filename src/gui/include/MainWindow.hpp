51f778c52a89b22bd4bf3097b41c55f3a20e7082
Daniil <shprotokot@gmail.com>
2026-03-04 00:54:21 +0300

    Add TaskWindow pointer and module navigation methods to MainWindow

--- src/gui/include/MainWindow.hpp
+++ src/gui/include/MainWindow.hpp
@@ -13,6 +13,7 @@ class QVBoxLayout;
 class QHBoxLayout;
 class QScrollArea;
 class CustomTitleBar;
+class TaskWindow;
 
 class MainWindow : public QWidget
 {
@@ -24,9 +25,18 @@ public:
 
 protected:
     void paintEvent(QPaintEvent *event) override;
+    void showEvent(QShowEvent* event) override;
 
 public slots:
     void fadeIn();
+    void fadeOut();
+    void openTaskWindow(int moduleId = 1);
+
+private slots:
+    void onModuleButtonClicked();
+    void onLearnButtonClicked();
+    void onTaskWindowClosed();
+    void updateModuleProgress(int moduleId, int progress);
 
 private:
     void setupUI();
@@ -36,11 +46,14 @@ private:
     void setupCenterPanel();        
     void setupRightPanel();     
     void setupStyles();
+    void setupConnections();
     void centerWindow();
+    void animateToTaskWindow(int moduleId);
 
     std::unique_ptr<CustomTitleBar> customTitleBar_;
     std::unique_ptr<QPropertyAnimation> transitionAnimation_;
     std::unique_ptr<QVBoxLayout> centerPanelLayout_;
+    std::unique_ptr<TaskWindow> taskWindow_;
     
     std::unique_ptr<QFrame> sideBar;
     std::unique_ptr<QFrame> eventCard;
@@ -59,4 +72,8 @@ private:
     QPushButton* aboutBtn;
     QPushButton* contactsBtn;
     QPushButton* privacyBtn;
+    QPushButton* learnBtn;
+    
+    bool isTransitioning_;
+    int pendingModuleId_;
 };
