eed8fc51a30bd851cff56e66a6ee7614c6a8d082
Daniil <shprotokot@gmail.com>
2026-03-15 16:28:41 +0300

    refactor(gui): modernize CustomTitleBar header with forward declarations and nullptr

--- src/gui/include/CustomTitleBar.hpp
+++ src/gui/include/CustomTitleBar.hpp
@@ -1,27 +1,30 @@
 #pragma once
 
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QMouseEvent>
-#include <QPoint>
-#include <QPushButton>
 #include <QWidget>
+#include <QPoint>
+
+class QHBoxLayout;
+class QLabel;
+class QPushButton;
+class QMouseEvent;
+class QIcon;
 
 class CustomTitleBar : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit CustomTitleBar(QWidget *parent = nullptr);
+    explicit CustomTitleBar(QWidget* parent = nullptr);
+    ~CustomTitleBar() override = default; 
 
-    void setTitle(const QString &title);
-    void setIcon(const QIcon &icon);
+    void setTitle(const QString& title);
+    void setIcon(const QIcon& icon);
 
 protected:
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseDoubleClickEvent(QMouseEvent *event) override;
-    bool eventFilter(QObject *obj, QEvent *event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mouseDoubleClickEvent(QMouseEvent* event) override;
+    bool eventFilter(QObject* obj, QEvent* event) override;
 
 private slots:
     void onMinimizeClicked();
@@ -32,12 +35,12 @@ private:
     void setupUI();
     void updateMaximizeRestoreIcon();
 
-    QLabel *iconLabel_;
-    QLabel *titleLabel_;
-    QPushButton *minimizeButton_;
-    QPushButton *maximizeRestoreButton_;
-    QPushButton *closeButton_;
-    QHBoxLayout *layout_;
+    QLabel* iconLabel_ = nullptr;
+    QLabel* titleLabel_ = nullptr;
+    QPushButton* minimizeButton_ = nullptr;
+    QPushButton* maximizeRestoreButton_ = nullptr;
+    QPushButton* closeButton_ = nullptr;
+    QHBoxLayout* layout_ = nullptr;
 
     QPoint dragPosition_;
     bool isMaximized_ = false;
