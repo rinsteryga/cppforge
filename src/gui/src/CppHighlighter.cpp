6e479f7fb12adc8ab26735c0b840c629c545ffc5
Daniil <shprotokot@gmail.com>
2026-03-18 21:55:25 +0300

    style: manually fix CppHighlighter to match clang-format

--- src/gui/src/CppHighlighter.cpp
+++ src/gui/src/CppHighlighter.cpp
@@ -6,17 +6,13 @@ CppHighlighter::CppHighlighter(QTextDocument *parent) : QSyntaxHighlighter(paren
     keywordFormat.setForeground(Qt::darkBlue);
     keywordFormat.setFontWeight(QFont::Bold);
     QStringList keywordPatterns;
-    keywordPatterns << "\\bchar\\b" << "\\bclass\\b" << "\\bconst\\b"
-                    << "\\bdouble\\b" << "\\benum\\b" << "\\bexplicit\\b"
-                    << "\\bfriend\\b" << "\\binline\\b" << "\\bint\\b"
-                    << "\\blong\\b" << "\\bnamespace\\b" << "\\boperator\\b"
-                    << "\\bprivate\\b" << "\\bprotected\\b" << "\\bpublic\\b"
-                    << "\\bshort\\b" << "\\bsignals\\b" << "\\bslots\\b"
-                    << "\\bstatic\\b" << "\\bstruct\\b" << "\\btemplate\\b"
-                    << "\\btypedef\\b" << "\\btypename\\b" << "\\bunion\\b"
-                    << "\\bunsigned\\b" << "\\bvirtual\\b" << "\\bvoid\\b"
-                    << "\\bvolatile\\b" << "\\bbool\\b" << "\\bif\\b"
-                    << "\\belse\\b" << "\\bfor\\b" << "\\bwhile\\b" << "\\breturn\\b";
+    keywordPatterns << "\\bchar\\b" << "\\bclass\\b" << "\\bconst\\b" << "\\bdouble\\b" << "\\benum\\b"
+                    << "\\bexplicit\\b" << "\\bfriend\\b" << "\\binline\\b" << "\\bint\\b" << "\\blong\\b"
+                    << "\\bnamespace\\b" << "\\boperator\\b" << "\\bprivate\\b" << "\\bprotected\\b" << "\\bpublic\\b"
+                    << "\\bshort\\b" << "\\bsignals\\b" << "\\bslots\\b" << "\\bstatic\\b" << "\\bstruct\\b"
+                    << "\\btemplate\\b" << "\\btypedef\\b" << "\\btypename\\b" << "\\bunion\\b" << "\\bunsigned\\b"
+                    << "\\bvirtual\\b" << "\\bvoid\\b" << "\\bvolatile\\b" << "\\bbool\\b" << "\\bif\\b" << "\\belse\\b"
+                    << "\\bfor\\b" << "\\bwhile\\b" << "\\breturn\\b";
 
     for (const QString &pattern : keywordPatterns)
     {
