e43d7a960df217a95caff347882de771ba5c50b3
iluha481 <paionnonionn@gmail.com>
2026-04-09 23:14:07 +0300

    added generateSalt signature and changed generate and verify signatures

--- src/core/include/utils/security/PasswordHashGenerator.hpp
+++ src/core/include/utils/security/PasswordHashGenerator.hpp
@@ -17,16 +17,25 @@ namespace cppforge::utils::security
         /**
          * @brief Transforms a raw password string into a secure cryptographic hash.
          * @param password The clear-text password to be handled.
+         * @param salt The salt to be used for password hashing.
          * @return A QString containing the resulting secure hash.
          */
-        static QString generate(const QString &password);
+        static QString generate(const QString &password, const QString &salt);
 
         /**
          * @brief Validates a raw password against an existing secure hash.
          * @param password The raw password input to check.
          * @param hash The previously generated secure hash.
+         * @param salt The salt to be used for password hashing.
          * @return True if the password matches the hash; otherwise false.
          */
-        static bool verify(const QString &password, const QString &hash);
+        static bool verify(const QString &password, const QString &hash, const QString &salt);
+
+        /**
+         * @brief Generates a random salt for password hashing.
+         * @param length The length of the salt to generate.
+         * @return A QString containing the generated salt.
+         */
+        static QString generateSalt(int length = 16);
     };
 } // namespace cppforge::utils::security
