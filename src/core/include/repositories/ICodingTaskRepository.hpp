e7a9b6c121a0c6f937c77a03d3f94aa133bfa2ca
rinsteryga <epicrusla262@gmail.com>
2026-04-21 23:37:51 +0300

    refactor(core): replace getRandomDuelTaskByLessonId with getRandomDuelTaskByTopic in interface

--- src/core/include/repositories/ICodingTaskRepository.hpp
+++ src/core/include/repositories/ICodingTaskRepository.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <optional>
 #include <vector>
+#include <QString>
 
 namespace cppforge
 {
@@ -48,12 +49,12 @@ namespace cppforge
             virtual std::optional<entities::CodingTask> getTaskById(uint64_t taskId) const = 0;
 
             /**
-             * @brief Fetches a random duel coding task.
+             * @brief Fetches a random duel coding task by topic.
              *
-             * @param lessonId The unique identifier of the lesson.
+             * @param topic The topic to filter duel tasks by.
              * @return An std::optional containing the CodingTask if found; otherwise std::nullopt.
              */
-            virtual std::optional<entities::CodingTask> getRandomDuelTaskByLessonId(uint64_t lessonId) const = 0;
+            virtual std::optional<entities::CodingTask> getRandomDuelTaskByTopic(const QString& topic) const = 0;
         };
     } // namespace repositories
 } // namespace cppforge
