aea8040ca0eca0618d2a4d85e6c8cb4324959ab7
rinsteryga <epicrusla262@gmail.com>
2026-04-30 19:09:30 +0300

    fix(core): update level completion condition for achievements

--- src/core/src/services/AchievementService.cpp
+++ src/core/src/services/AchievementService.cpp
@@ -23,7 +23,7 @@ namespace cppforge::services
         using ValidatorFn = std::function<bool(const entities::User &, uint32_t)>;
         std::unordered_map<entities::ConditionType, ValidatorFn> validators = {
             {entities::ConditionType::LevelsCompleted,
-             [](const entities::User &user, uint32_t val) { return user.getCompletedLevelsIds().size() >= val; }},
+             [&](const entities::User &user, uint32_t val) { return userRepo_.getSolvedTasksCount(user.getId()) >= static_cast<int>(val); }},
             {entities::ConditionType::StreakDays,
              [](const entities::User &user, uint32_t val) { return user.getCurrentStreakDays() >= val; }},
             {entities::ConditionType::CustomEvent, [](const entities::User &, uint32_t) { return false; }}};
